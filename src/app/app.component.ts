e7dc6e5c42766f85ea434da927e9263ee7972c36
Gabriel Papke Paganini <gabrielpapkepaganini@192.168.0.6>
2025-04-01 09:35:09 -0300

    feat: initial structure

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from './components/menu/menu.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    RouterOutlet,
+    MenuComponent,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'out-challenge';
+}
