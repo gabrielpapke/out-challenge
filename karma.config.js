e8f89780150b17e485791b1e23ace86423e1a399
Gabriel Papke Paganini <gabrielpapkepaganini@192.168.0.6>
2025-04-06 20:07:19 -0300

    build: karma config

---
+++ karma.config.js
@@ -0,0 +1,11 @@
+module.exports = function (config) {
+  config.set({
+    browsers: ['ChromeHeadless'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-setuid-sandbox'],
+      },
+    },
+  });
+};
