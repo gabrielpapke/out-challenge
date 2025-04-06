d215cbe4adc29d08b7bade9feb9276eaa3244a4f
Gabriel Papke Paganini <gabrielpapkepaganini@192.168.0.6>
2025-04-06 01:17:31 -0300

    feat: add state component

---
+++ src/app/ui/state/state.component.ts
@@ -0,0 +1,19 @@
+import { Component, computed, input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-state',
+  imports: [MatCardModule, MatIconModule, MatButtonModule],
+  templateUrl: './state.component.html',
+  styleUrl: './state.component.scss',
+})
+export class StateComponent {
+  type = input.required<'info' | 'error'>();
+  title = input.required<string>();
+
+  icon = computed(() =>
+    this.type() === 'error' ? 'error' : 'check_box_outline_blank'
+  );
+}
--- src/app/ui/table/table.component.module.ts
+++ src/app/ui/table/table.component.module.ts
@@ -2,12 +2,19 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { SpinnerComponent } from '@ui/spinner/spinner.component';
+import { StateComponent } from '@ui/state/state.component';
 import { ShimmerComponent } from '../shimmer/shimmer.component';
 import { TableComponent } from './table.component';
 
 @NgModule({
   declarations: [TableComponent],
-  imports: [CommonModule, MatTableModule, ShimmerComponent, SpinnerComponent],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    ShimmerComponent,
+    SpinnerComponent,
+    StateComponent,
+  ],
   exports: [TableComponent, MatTableModule],
 })
 export class TableModule {}
