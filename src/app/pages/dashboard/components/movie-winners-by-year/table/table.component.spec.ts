307cba322bdb976fab4d39ee28a2ab6f007bdd32
Gabriel Papke Paganini <gabrielpapkepaganini@192.168.0.6>
2025-04-07 21:30:19 -0300

    feat: add winner-by-year test

--- src/app/pages/dashboard/components/movie-winners-by-year/table/table.component.spec.ts
+++ src/app/pages/dashboard/components/movie-winners-by-year/table/table.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { By } from '@angular/platform-browser';
 import { moviesMock } from '@mocks/movies-content.mock';
+import { TableComponent } from '@ui/table/table.component';
 import { MovieWinnersByYearTableComponent } from './table.component';
 
 describe('MovieWinnersByYearTableComponent', () => {
@@ -14,13 +16,47 @@ describe('MovieWinnersByYearTableComponent', () => {
 
     fixture = TestBed.createComponent(MovieWinnersByYearTableComponent);
     component = fixture.componentInstance;
-  });
 
-  it('should create', () => {
+    spyOn(component.onRetry, 'emit');
+
     fixture.componentRef.setInput('data', structuredClone(moviesMock));
     fixture.componentRef.setInput('loading', false);
     fixture.componentRef.setInput('hasError', false);
+  });
+
+  it('should create', () => {
     fixture.detectChanges();
     expect(component).toBeTruthy();
+  });
+
+  it('should pass data to app table', () => {
+    fixture.detectChanges();
+
+    const table = fixture.debugElement.query(By.directive(TableComponent));
+    const { data, loading, hasError } =
+      table.componentInstance as TableComponent;
+
+    expect(data()).toEqual(structuredClone(moviesMock));
+    expect(loading()).toEqual(false);
+    expect(hasError()).toEqual(false);
+  });
+
+  it('should pass loading to app table', () => {
+    fixture.componentRef.setInput('loading', true);
+    fixture.detectChanges();
+
+    const table = fixture.debugElement.query(By.directive(TableComponent));
+    const { loading } = table.componentInstance as TableComponent;
+
+    expect(loading()).toEqual(true);
+  });
+
+  it('should call onRetry', () => {
+    fixture.detectChanges();
+
+    const table = fixture.debugElement.query(By.directive(TableComponent));
+    table.triggerEventHandler('onRetry', undefined);
+
+    expect(component.onRetry.emit).toHaveBeenCalled();
   });
 });
