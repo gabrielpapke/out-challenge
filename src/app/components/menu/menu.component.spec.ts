52d89b188f1b3749a539d068fa3f23459bb08dbf
Gabriel Papke Paganini <gabrielpapkepaganini@192.168.0.6>
2025-04-07 21:11:33 -0300

    feat: add menu test

--- src/app/components/menu/menu.component.spec.ts
+++ src/app/components/menu/menu.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { MatListItem } from '@angular/material/list';
+import { By } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { MenuComponent } from './menu.component';
 
@@ -15,10 +17,34 @@ describe('MenuComponent', () => {
 
     fixture = TestBed.createComponent(MenuComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+
+    spyOn(component.onCloseButtonClick, 'emit');
+    spyOn(component.onMenuItemClick, 'emit');
   });
 
   it('should create', () => {
+    fixture.detectChanges();
+
     expect(component).toBeTruthy();
+  });
+
+  it('should emit close menu on click', () => {
+    fixture.detectChanges();
+
+    const closeMenuButton = fixture.debugElement.query(
+      By.css('[data-test-id=close-menu]')
+    );
+    closeMenuButton.nativeElement.click();
+
+    expect(component.onCloseButtonClick.emit).toHaveBeenCalled();
+  });
+
+  it('should emit item menu click', () => {
+    fixture.detectChanges();
+
+    const itemMenu = fixture.debugElement.query(By.directive(MatListItem));
+    itemMenu.nativeElement.click();
+
+    expect(component.onMenuItemClick.emit).toHaveBeenCalled();
   });
 });
