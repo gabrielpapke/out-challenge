6a46af3f05ec5b233c0bf09936a2ce57d43ea7a1
Gabriel Papke Paganini <gabrielpapkepaganini@192.168.0.6>
2025-04-07 21:38:50 -0300

    feat: add yes-no pipe test

--- src/app/pipes/yes-no.pipe.spec.ts
+++ src/app/pipes/yes-no.pipe.spec.ts
@@ -5,4 +5,18 @@ describe('YesNoPipe', () => {
     const pipe = new YesNoPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should return "Yes" description', () => {
+    const pipe = new YesNoPipe();
+
+    const result = pipe.transform(true);
+    expect(result).toEqual('Yes');
+  });
+
+  it('should return "No" description', () => {
+    const pipe = new YesNoPipe();
+
+    const result = pipe.transform(false);
+    expect(result).toEqual('No');
+  });
 });
