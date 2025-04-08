22820733e7e942f605197bbc543bf2e993f31386
Gabriel Papke Paganini <gabrielpapkepaganini@192.168.0.6>
2025-04-07 21:15:33 -0300

    feat: add toolbar test

--- src/app/components/toolbar/toolbar.component.spec.ts
+++ src/app/components/toolbar/toolbar.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { MatIconButton } from '@angular/material/button';
+import { By } from '@angular/platform-browser';
 import { ToolbarComponent } from './toolbar.component';
 
 describe('ToolbarComponent', () => {
@@ -8,16 +10,26 @@ describe('ToolbarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ToolbarComponent]
-    })
-    .compileComponents();
+      imports: [ToolbarComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ToolbarComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+
+    spyOn(component.onMenuClick, 'emit');
   });
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
+  });
+
+  it('should emit event menu on click', () => {
+    fixture.detectChanges();
+
+    const menuButton = fixture.debugElement.query(By.directive(MatIconButton));
+    menuButton.nativeElement.click();
+
+    expect(component.onMenuClick.emit).toHaveBeenCalled();
   });
 });
